d848afac901587a8b2c3c482dbbefa8abcecd74a
Max G <user@MacBook-Pro.local>
2025-02-12 22:33:05 +0200

    firs

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Welcome.scss";
+import Logo from "../assets/whitepuer.svg";
 
 const Welcome = () => {
   const [showLogo, setShowLogo] = useState(false);
@@ -16,7 +17,7 @@ const Welcome = () => {
       <svg className="welcome-text" viewBox="0 0 500 100">
         <text x="50%" y="50%" text-anchor="middle" dy=".3em">Welcome to</text>
       </svg>
-      <img className={showLogo ? "slide-in" : ""} src="src/assets/whitelogo.svg" alt="Puer Logo" />
+      <img className={showLogo ? "slide-in" : ""} src={Logo} alt="Puer Logo" />
     </div>
   );
 };
