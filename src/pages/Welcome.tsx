71eb6571519f4d327e66ad835bf95a9c92a2c064
Max G <user@MacBook-Pro.local>
2025-02-14 10:39:38 +0200

    test2

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -8,14 +8,14 @@ const Welcome = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setTimeout(() => setShowLogo(true), 1500); // Логотип появляется с эффектом
-    setTimeout(() => navigate("/food/hot"), 4000); // Переход на главную страницу
+    setTimeout(() => setShowLogo(true), 2000); // Логотип появляется через 2 сек
+    setTimeout(() => navigate("/food/hot"), 5000); // Переход через 5 сек
   }, [navigate]);
 
   return (
     <div className="welcome">
-      <svg className="welcome-text" viewBox="0 0 500 100">
-        <text x="50%" y="50%" text-anchor="middle" dy=".3em">Welcome to</text>
+      <svg className="hello-text" viewBox="0 0 500 100">
+        <text x="50%" y="50%" textAnchor="middle" dy=".3em">Welcome</text>
       </svg>
       <img className={showLogo ? "slide-in" : ""} src={Logo} alt="Puer Logo" />
     </div>
