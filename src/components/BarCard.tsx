8171832a24f1cde91b822bc8581d2f89e8ea6b75
Max G <user@MacBook-Pro.local>
2025-02-15 13:58:18 +0200

    test2

--- src/components/BarCard.tsx
+++ src/components/BarCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./ProductCard.scss";
+import { useTranslation } from 'react-i18next';
 
 interface ProductProps {
   image: string;
@@ -12,6 +13,7 @@ interface ProductProps {
 const getImagePath = (image: string) => new URL(`/src/assets/${image}`, import.meta.url).href;
 
 const BarCard = ({ image, name, description, weight, price }: ProductProps) => {
+  const { t } = useTranslation();
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const openPopup = () => {
@@ -29,8 +31,8 @@ const BarCard = ({ image, name, description, weight, price }: ProductProps) => {
       <img src={getImagePath(image)} alt={name} className="product-image" />
         <h3 className="product-name">{name}</h3>
         <p className="product-description">{description}</p>
-        {weight && <p className="product-weight">Объем: {weight} ml</p>} {/* Показываем вес, если он есть */}
-        <p className="product-price">Цена: {price} Lei</p>
+        {weight && <p className="product-weight">{t('bar.weight')}: {weight} {t('bar.ml')}</p>} {/* Показываем вес, если он есть */}
+        <p className="product-price">{t('product.price')}: {price} {t('product.currency')}</p>
       </div>
 
       {isPopupOpen && (
@@ -42,8 +44,8 @@ const BarCard = ({ image, name, description, weight, price }: ProductProps) => {
             <img src={getImagePath(image)} alt={name} className="product-image" />
             <h3 className="popup-name">{name}</h3>
             <p className="popup-description">{description}</p>
-            {weight && <p className="popup-weight">Объем: {weight} ml</p>} {/* Показываем вес в попапе, если он есть */}
-            <p className="popup-price">Цена: {price} Lei</p>
+            {weight && <p className="popup-weight">{t('bar.weight')}: {weight} {t('bar.ml')}</p>} {/* Показываем вес в попапе, если он есть */}
+            <p className="popup-price">{t('product.price')}: {price} {t('product.currency')}</p>
           </div>
         </div>
       )}
