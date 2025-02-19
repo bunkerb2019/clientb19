c0249cbc1d6bcdbbd5f323baa9aaf8a060e04f7f
Max G <user@MacBook-Pro.local>
2025-02-19 23:17:15 +0200

    welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Welcome.scss";
-// import Logo from "../assets/whitelogo.svg";
 import Logo from "../assets/demoversionpuerlogo.svg";
-
 
 const Welcome = () => {
   const [showLogo, setShowLogo] = useState(false);
+  const [hideScreen, setHideScreen] = useState(false);
   const navigate = useNavigate();
+
   useEffect(() => {
-    
     setTimeout(() => setShowLogo(true), 2000); // Логотип появляется через 2 сек
-    setTimeout(() => navigate("/food/rolls"), 5000); // Переход через 5 сек
+
+    setTimeout(() => {
+      setHideScreen(true); // Запуск анимации исчезновения
+      setTimeout(() => navigate("/food/rolls"), 1000); // Переход через 1 сек после начала скрытия
+    }, 5000);
   }, [navigate]);
 
   return (
-    <div className="welcome">
+    <div className={`welcome ${hideScreen ? "fade-out" : ""}`}>
       <svg className="hello-text" viewBox="0 0 500 100">
-        
-        <text x="50%" y="50%" textAnchor="middle" dy=".3em">Welcome to</text>
+        <text x="50%" y="50%" textAnchor="middle" dy=".3em">
+          Welcome to
+        </text>
       </svg>
       <img className={showLogo ? "slide-in" : ""} src={Logo} alt="Puer Logo" />
     </div>
