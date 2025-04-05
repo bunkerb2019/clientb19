0e85b0489f1a7df3e9e8a375e4d488373da3d729
VSuvorov <end17104@adobe.com>
2025-04-05 17:01:46 +0300

    added env vars

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // 🔥 Твой конфиг Firebase (замени своими данными)
 const firebaseConfig = {
-    apiKey: "AIzaSyBwes6yd-24ngRBBEaIs8SXGjd6Li86YbU",
-    authDomain: "main-menu-e6ed6.firebaseapp.com",
-    projectId: "main-menu-e6ed6",
-    storageBucket: "main-menu-e6ed6.firebasestorage.app",
-    messagingSenderId: "861499763984",
-    appId: "1:861499763984:web:eb00ba63de7368ea3e8995",
-    measurementId: "G-51DMHXXH1X"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASURMENT_ID,
   };
 
 // Инициализация Firebase
